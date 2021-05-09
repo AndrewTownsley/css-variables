bbc6cf1e8d5ff46c91b73e30176e09e711ddf18d
Andrew TOwnsley <atownsley86@gmail.com>
2021-05-09 17:10:16 -0600

    app started

---
+++ app.js
@@ -0,0 +1,7 @@
+// Declare Variables
+// spacoing
+const spacing = document.getElementById('spacing');
+const blur = document.getElementById('blur');
+const base = document.getElementById('base');
+//blur
+//base
